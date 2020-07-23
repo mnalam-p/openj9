80217f08fd027bde3e810c2162a2408137810269
Mohammad Nazmul Alam <mohammad.nazmul.alam@ibm.com>
2020-07-23 14:46:37 -0400

    for testing
    Signed-off-by: Mohammad Nazmul Alam <mohammad.nazmul.alam@ibm.com>

--- runtime/compiler/optimizer/J9CFGSimplifier.cpp
+++ runtime/compiler/optimizer/J9CFGSimplifier.cpp
@@ -34,13 +34,13 @@
 
 bool J9::CFGSimplifier::simplifyIfPatterns(bool needToDuplicateTree)
    {
-   static char *enableCFGSimplification = feGetEnv("TR_enableCFGSimplificaiton");
+   /*static char *enableCFGSimplification = feGetEnv("TR_enableCFGSimplificaiton");
    if (enableCFGSimplification == NULL)
-      return false;
+      return false;*/
 
    return OMR::CFGSimplifier::simplifyIfPatterns(needToDuplicateTree)
-          || simplifyResolvedRequireNonNull(needToDuplicateTree)
-          || simplifyUnresolvedRequireNonNull(needToDuplicateTree)
+          //|| simplifyResolvedRequireNonNull(needToDuplicateTree)
+          //|| simplifyUnresolvedRequireNonNull(needToDuplicateTree)
           ;
    }
 
